1a2d2d2e43535d7ce9679e10638c7d7dddc96bda
Brandon Frie <brandon@brandonfrie.com>
2020-04-15 02:42:54 -0500

    Added the ability to switch between JNSQ and stock maps.

---
+++ WebPages/WebPages/src/js/maps/layer-defs.js
@@ -0,0 +1,36 @@
+﻿const baseUrl = 'https://d3kmnwgldcmvsd.cloudfront.net';
+
+class TelemachusMaps {
+    layerDefinitions = [
+        {
+            name: 'Kerbin Satellite (JNSQ)',
+            layer: this.generateJnsqLayer({ service: 'jnsq', body: 'kerbin', style: 'sat' })
+        },
+        {
+            name: 'Kerbin Satellite (Stock)',
+            layer: this.generateStockLayer({ body: 'kerbin', style: 'sat' })
+        }
+    ];
+
+    generateLayer(url, layerDefinition) {
+        return new L.tileLayer(`${url}/tiles/${layerDefinition.body}/${layerDefinition.style}/{z}/{x}/{y}.png`, {
+            attribution: '&copy; <a href="https://kerbal-maps.finitemonkeys.org/">Kerbal Maps</a>',
+            tms: true
+        });
+    };
+
+    generateJnsqLayer(layerDefinition) {
+        return this.generateLayer(`${baseUrl}/jnsq`, layerDefinition);
+    };
+
+    generateStockLayer(layerDefinition) {
+        return this.generateLayer(baseUrl, layerDefinition);
+    };
+
+    layerDefinitionsAsObject() {
+        return this.layerDefinitions.reduce((obj, item) => {
+            obj[item.name] = item.layer;
+            return obj;
+        }, {});
+    };
+}
